73408de54f8eecb6710534b7ebbef4c9c1686e64
Paige Godfrey <paagodfrey@gmail.com>
2020-04-21 22:41:59 -0700

    fixed link issue

--- frontend/src/CompanyCard.js
+++ frontend/src/CompanyCard.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Link, BrowserRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import defaultLogo from "./default-logo.png";
 import './Card.css';
 
 const CompanyCard = ({ company = {} }) => {
   const { name, description, logo_url, handle } = company;
   return (
-    <BrowserRouter>
-      <Link className="CompanyCard Card card" to={`/companies/${handle}`}>
-        <div className="card-body">
-          <h6 className="card-title d-flex justify-content-between">
-            <span className="text-capitalize">{name}</span>
-            <img src={logo_url || defaultLogo} alt={`${name} Logo`} />
-          </h6>
-          <p>{description}</p>
-        </div>
-      </Link>
-    </BrowserRouter>
+    <Link className="CompanyCard Card card" to={`/companies/${handle}`}>
+      <div className="card-body">
+        <h6 className="card-title d-flex justify-content-between">
+          <span className="text-capitalize">{name}</span>
+          <img src={logo_url || defaultLogo} alt={`${name} Logo`} />
+        </h6>
+        <p>{description}</p>
+      </div>
+    </Link>
   );
 }
 
