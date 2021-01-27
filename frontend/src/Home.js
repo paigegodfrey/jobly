9337a8d8fa8b2219571178cda1d0ac9150fb28b2
Paige Godfrey <paagodfrey@gmail.com>
2021-01-27 14:56:03 -0800

    consistent bootstrap format row/col

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,4 @@
 import React, { useContext } from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { Link } from "react-router-dom";
 import UserContext from "./UserContext";
 import headerImg from "./you-x-ventures-X8H8vPcelPk-unsplash.jpg";
@@ -11,14 +8,14 @@ const Home = () => {
   const { currentUser } = useContext(UserContext);
   return (
     <div id="hero" className="Home d-flex">
-      <Container className="hero-container">
-        <Row>
-          <Col lg={7}>
+      <div className="container hero-container">
+        <div className="row">
+          <div className="col-lg-7">
             <div>
               <img className="img-fluid" src={headerImg} alt="jobly" />
             </div>
-          </Col>
-          <Col lg={5} className="title-text-col d-flex align-items-center mt-4 mt-lg-0 ml-auto">
+          </div>
+          <div className="title-text-col d-flex align-items-center col-lg-5 mt-4 mt-lg-0 ml-auto">
             <div>
               <h1>Jobly</h1>
               <p className="lead">All the jobs in one, convenient place.</p>
@@ -36,9 +33,9 @@ const Home = () => {
                   </div>
                 )}
             </div>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
